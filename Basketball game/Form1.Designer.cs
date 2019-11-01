2e2d7b126638dbffd491820f3e4df20c25c6fe81
Zhi Cheng Wu <zhiwu093@ed.amdsb.ca>
2019-11-01 12:31:48 -0400

    Add project files.

---
+++ Basketball game/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Basketball_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.pictureRed = new System.Windows.Forms.PictureBox();
+            this.pictureBlue = new System.Windows.Forms.PictureBox();
+            this.pictureYellow = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureYellow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.outputLabel.Location = new System.Drawing.Point(390, 39);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(356, 79);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "What player do your want to be?";
+            // 
+            // redLabel
+            // 
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(468, 166);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(293, 58);
+            this.redLabel.TabIndex = 1;
+            this.redLabel.Text = "Lebron James";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(468, 237);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(225, 54);
+            this.blueLabel.TabIndex = 2;
+            this.blueLabel.Text = "Kyrie Ivring";
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.ForeColor = System.Drawing.Color.White;
+            this.yellowLabel.Location = new System.Drawing.Point(468, 311);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(189, 52);
+            this.yellowLabel.TabIndex = 3;
+            // 
+            // pictureRed
+            // 
+            this.pictureRed.BackgroundImage = global::Basketball_game.Properties.Resources.red_50x50;
+            this.pictureRed.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureRed.Location = new System.Drawing.Point(340, 166);
+            this.pictureRed.Name = "pictureRed";
+            this.pictureRed.Size = new System.Drawing.Size(100, 50);
+            this.pictureRed.TabIndex = 4;
+            this.pictureRed.TabStop = false;
+            // 
+            // pictureBlue
+            // 
+            this.pictureBlue.BackgroundImage = global::Basketball_game.Properties.Resources.blue_50x50;
+            this.pictureBlue.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBlue.Location = new System.Drawing.Point(340, 237);
+            this.pictureBlue.Name = "pictureBlue";
+            this.pictureBlue.Size = new System.Drawing.Size(100, 50);
+            this.pictureBlue.TabIndex = 5;
+            this.pictureBlue.TabStop = false;
+            // 
+            // pictureYellow
+            // 
+            this.pictureYellow.BackgroundImage = global::Basketball_game.Properties.Resources.yellow_50x50;
+            this.pictureYellow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureYellow.Location = new System.Drawing.Point(340, 311);
+            this.pictureYellow.Name = "pictureYellow";
+            this.pictureYellow.Size = new System.Drawing.Size(100, 50);
+            this.pictureYellow.TabIndex = 6;
+            this.pictureYellow.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureYellow);
+            this.Controls.Add(this.pictureBlue);
+            this.Controls.Add(this.pictureRed);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureYellow)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.PictureBox pictureRed;
+        private System.Windows.Forms.PictureBox pictureBlue;
+        private System.Windows.Forms.PictureBox pictureYellow;
+    }
+}
+
---
+++ Basketball game/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basketball_game
+{
+    public partial class Form1 : Form
+    {
+        int scene = 1;
+        Random ranGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 1) { scene = 3; }
+                else if (scene == 3) { scene = 7; }
+                else if (scene == 7) { scene = 16; }
+                else if (scene == 16) { scene = 29; }
+                else if (scene == 29)
+                {
+                    int randValue = ranGen.Next(1, 101);
+                    if (randValue < 90) { scene = 38; }
+                    else { scene = 37; }
+                }
+                else if (scene == 14) { scene = 25; }
+                else if (scene == 24)
+                {
+                    int randValue = ranGen.Next(1, 101);
+                    if (randValue < 70) { scene = 34; }
+                    else { scene = 33; }
+                }
+                else if (scene == 8) { scene = 13; }
+                else if (scene == 13) { scene = 23; }
+                else if (scene == 2) { scene = 5; }
+                else if (scene == 5) { scene = 11; }
+                else if (scene == 11)
+                {
+                    int randValue = ranGen.Next(1, 101);
+
+                    if (randValue < 80) { scene = 99; }
+                    else { scene = 21; }
+                }
+                else if (scene == 10) { scene = 20; }
+                else if (scene == 19) { scene = 98; }
+                else if (scene == 4) { scene = 9; }
+                else if (scene == 8) { scene = 18; }
+
+
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 1) { scene = 2; }
+                else if (scene == 2) { scene = 4; }
+                else if (scene == 4) { scene = 8; }
+                else if (scene == 8) { scene = 17; }
+                else if (scene == 5) { scene = 10; }
+                else if (scene == 10) { scene = 19; }
+                else if (scene == 19) { scene = 99; }
+                else if (scene == 3) { scene = 6; }
+                else if (scene == 6) { scene = 12; }
+                else if (scene == 13) { scene = 22; }
+                else if (scene == 7) { scene = 14; }
+                else if (scene == 14) { scene = 24; }
+                else if (scene == 24) { scene = 32; }
+                else if (scene == 16)
+                {
+                    int randValue = ranGen.Next(1, 101);
+
+                    if (randValue < 75) { scene = 99; }
+                    else { scene = 98; }
+                }
+                else if (scene == 29)
+                {
+                    int randValue = ranGen.Next(1, 101);
+
+                    if (randValue < 40) { scene = 99; }
+                    else { scene = 98; }
+                }
+            
+            else if (e.KeyCode == Keys.N)
+            {
+                if (scene == 7) { scene = 15; }
+
+
+                else if (scene == 17) { scene = 98; }
+                else if (scene == 18) { scene = 99; }
+                else if (scene == 9) { scene = 98; }
+                else if (scene == 20) { scene = 98; }
+                else if (scene == 21) { scene = 98; }
+                else if (scene == 12) { scene = 98; }
+                else if (scene == 22) { scene = 98; }
+                else if (scene == 23) { scene = 99; }
+                else if (scene == 32) { scene = 99; }
+                else if (scene == 33) { scene = 98; }
+                else if (scene == 34) { scene = 99; }
+                else if (scene == 25) { scene = 99; }
+                else if (scene == 15) { scene = 98; }
+                else if (scene == 37) { scene = 98; }
+                else if (scene == 38) { scene = 99; }
+            }
+
+            }
+
+
+            switch (scene)
+            {
+                
+                case 1:
+                    outputLabel.Text = "What do you want to be?";
+                    redLabel.Text = "Lebron James";
+                    blueLabel.Text = "Kyrie Ivring";
+                    yellowLabel.Text = "";
+                    break;
+                case 2:
+                    outputLabel.Text = "Who do you want to play against?";
+                    redLabel.Text = "Chris Paul";
+                    blueLabel.Text = "Luka Doncic";
+                    break;
+                case 3:
+                    outputLabel.Text = "Who do you want to play against?";
+                    redLabel.Text = "Chris Paul";
+                    blueLabel.Text = "Luka Doncic";
+                    break;
+                case 4:
+                    outputLabel.Text = "Game to 1, you start with the ball. What do you want to do?";
+                    redLabel.Text = "Shoot it in his face";
+                    blueLabel.Text = "Dribble";
+                    break;
+                case 5:
+                    outputLabel.Text = "Game to 1, you start with the ball. What do you want to do?";
+                    redLabel.Text = "Shoot";
+                    blueLabel.Text = "Pump fake";
+                    break;
+                case 6:
+                    outputLabel.Text = "Game to 1, you start with the ball. What do you want to do?";
+                    redLabel.Text = "Drive";
+                    blueLabel.Text = "Pass";
+                    break;
+                case 7:
+                    outputLabel.Text = "Game to 1, you start with the ball. What do you want to do?";
+                    redLabel.Text = "Post up";
+                    blueLabel.Text = "Dribble";
+                    yellowLabel.Text = "Pass";
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                case 19:
+                    break;
+                case 20:
+                    break;
+                case 21:
+                    break;
+                case 22:
+                    break;
+                case 23:
+                    break;
+                case 24:
+                    break;
+                case 25:
+                    break;
+                case 26:
+                    break;
+                case 27:
+                    break;
+                case 28:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        
+    }
+}
